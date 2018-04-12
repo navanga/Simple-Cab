d94261d2266f7407bcdad09f32af166a6c2d0cab
navanga <navanga@gmail.com>
2018-04-12 16:56:58 +1000

    Added streams

--- client/src/main/java/com/datarepublic/simplecab/SimpleCabServiceImpl.java
+++ client/src/main/java/com/datarepublic/simplecab/SimpleCabServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,52 +16,45 @@ import java.util.Map;
 @Service("SimpleCabService")
 public class SimpleCabServiceImpl implements SimpleCabService {
 
-	@Autowired
+    @Autowired
     private RestTemplate restTemplate;
-	
-	@Value("${remote.host.url:http://localhost:8080/}")
-	private String serverUrl;
-	
-	@Override
-	public void deleteCache() {
-		ResponseEntity<String> response = restTemplate.getForEntity(serverUrl + "/resetCache", String.class);
-		System.out.println(response.getBody());
-		
-	}
 
-	@Override
-	public Map<String, Integer> getMedallionsSummary(String pickupDate, String... medallions) {
-		return getMedallionsSummary(pickupDate, false, medallions);
-	}
+    @Value("${remote.host.url:http://localhost:8080/}")
+    private String serverUrl;
 
-	@Override
-	public Map<String, Integer> getMedallionsSummary(String pickupDate, boolean ignoreCache, String... medallions) {
-		
-		List<String> parameters = new ArrayList<>();
-		
-		StringBuilder params = new StringBuilder();
-		for (String medallion : medallions) {
-			params.append("medallions=").append(medallion).append("&");
-			parameters.add("medallions=" + medallion);
-		}
-		params.append("pickupDate=").append(pickupDate).append("&");
-		params.append("ignoreCache=").append(ignoreCache);
-		
-		parameters.add("pickupDate=" + pickupDate);
-		parameters.add("ignoreCache=" + ignoreCache);
-		
-		parameters.stream().reduce((para, next) -> para + "&" + next);
-		
-		ResponseEntity<HashMap> response = restTemplate.getForEntity(serverUrl + "?" + params, HashMap.class);
-	
-		System.out.println(response.getBody());		
-				
-		return null;
-	}
-	
-	@Bean
-	public RestTemplate restTemplate() {
-	    return new RestTemplate();
-	}
+    @Override
+    public void deleteCache() {
+        ResponseEntity<String> response = restTemplate.getForEntity(serverUrl + "/resetCache", String.class);
+        System.out.println(response.getBody());
+
+    }
+
+    @Override
+    public Map<String, Integer> getMedallionsSummary(String pickupDate, String... medallions) {
+        return getMedallionsSummary(pickupDate, false, medallions);
+    }
+
+    @Override
+    public Map<String, Integer> getMedallionsSummary(String pickupDate, boolean ignoreCache, String... medallions) {
+
+        List<String> parameters = new ArrayList<>();
+
+        Arrays.stream(medallions).forEach(med -> parameters.add("medallions=" + med));
+        parameters.add("pickupDate=" + pickupDate);
+        parameters.add("ignoreCache=" + ignoreCache);
+
+        String params = parameters.stream().reduce((para, next) -> para + "&" + next).orElse("");
+
+        ResponseEntity<HashMap> response = restTemplate.getForEntity(serverUrl + "?" + params, HashMap.class);
+
+        System.out.println(response.getBody());
+
+        return null;
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
 }
